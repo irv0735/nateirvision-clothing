b093f2346b917808537e13fbe29a2025ee473605
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2022-05-15 18:12:23 -0600

    added some links to shop and sign-in pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 import { Routes, Route } from 'react-router-dom';
 
+
+const Shop = () => {
+  return (
+    <h1>I am the Shop page</h1>
+  )
+}
 
 const App = () => {
 
@@ -9,6 +16,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   )
