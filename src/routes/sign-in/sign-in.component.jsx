4b16580322da4c30336641ccc1bddcbdde79835d
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2022-05-18 09:57:55 -0600

    removed redirect option to stick with pop-up only

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,23 +1,11 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth'; 
-
 import {
-  auth, 
   signInWithGooglePopup,
-  signInWithGoogleRedirect, 
   createUserDocumentFromAuth 
 } from '../../utils/firebase/firebase.utils';
-import { connectFirestoreEmulator } from 'firebase/firestore';
+
 
 
 const SignIn = () => {
-
-  useEffect(async () => {
-    const response = await getRedirectResult(auth);
-    if(response) {
-      const userDocRef = await createUserDocumentFromAuth(response.user);
-    }
-  }, []);
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -30,9 +18,6 @@ const SignIn = () => {
       <button
         onClick={logGoogleUser}
       >Sign in with Google Popup</button>
-      <button
-        onClick={signInWithGoogleRedirect}
-      >Sign in with Google Redirect</button>
     </div>
   )
 }
