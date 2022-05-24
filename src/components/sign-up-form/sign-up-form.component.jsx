08efb284755e0839f0dde06e787dae61ac0dac10
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2022-05-24 13:45:57 -0600

    updated style on sign-up form and moved inputs to separate component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import FormInput from '../form-input/form-input.component';
 import { 
   createAuthUserWithEmailAndPassword, 
   createUserDocumentFromAuth 
@@ -50,33 +51,38 @@ const SignUpForm = () => {
     <div>
       <h1>Sign up with your email and password</h1>
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input 
-          type="text" 
-          required onChange={handleChange} 
-          name="displayName" 
-          value={displayName}/>
-        <label>Email</label>
-        <input 
-          type="email" 
+        <FormInput
+        label='Display Name'
+        type='text'
+        required
+        onChange={handleChange}
+        name='displayName'
+        value={displayName}
+        />
+        <FormInput 
+          label='Email'
+          type='email' 
           required 
           onChange={handleChange} 
-          name="email" 
-          value={email}/>
-        <label>Password</label>
-        <input 
-          type="password" 
+          name='email' 
+          value={email}
+        />
+        <FormInput
+          label='Password' 
+          type='password' 
           required 
           onChange={handleChange} 
-          name="password" 
-          value={password}/>
-        <label>Confirm Password</label>
-        <input 
-          type="password" 
+          name='password' 
+          value={password}
+        />
+        <FormInput 
+          label='Confirm Password'
+          type='password' 
           required 
           onChange={handleChange} 
-          name="confirmPassword" 
-          value={confirmPassword}/>
+          name='confirmPassword' 
+          value={confirmPassword}
+        />
         <button type="submit">Sign Up</button>
       </form>
     </div>
