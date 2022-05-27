09aff4a3befc7c4172ff4a6a27bfe7ebc5d23d0c
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2022-05-27 10:27:04 -0600

    moved data to firebase db

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useContext } from 'react';
+import { useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom'
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
@@ -18,7 +18,7 @@ const Navigation = () => {
   const { isCartOpen } = useContext(CartContext);
 
   return (
-    <Fragment>
+    <>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
           <CrwnLogo className='logo'/>
@@ -39,7 +39,7 @@ const Navigation = () => {
         {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
-    </Fragment>
+    </>
   )
 }
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,13 +6,18 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword, 
   signOut, 
-  onAuthStateChanged
+  onAuthStateChanged,
+  
 } from 'firebase/auth';
 import { 
   getFirestore,
   doc,
   getDoc,
-  setDoc
+  setDoc,
+  collection,
+  writeBatch, 
+  query,
+  getDocs
  } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -36,6 +41,32 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
 
 export const db = getFirestore();
+ 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd, field) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object[field].toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+  return categoryMap;
+}
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation) => {
   if(!userAuth) return;
@@ -43,6 +74,7 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
   const userSnapshot = await getDoc(userDocRef);
 
   if(!userSnapshot.exists()) {
+
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
