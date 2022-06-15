5b5aba175aff206de2530de37cf6861b368f603f
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2022-06-15 11:13:50 -0600

    redux-saga category conversion

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,6 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from './category.types';
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+// import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 export const fetchCategoriesStart = () => 
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
@@ -11,13 +11,13 @@ export const fetchCategoriesSucceed = (categoriesArray) =>
 export const fetchCategoriesFailed = (error) => 
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
 
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
+// export const fetchCategoriesAsync = () => async (dispatch) => {
+//   dispatch(fetchCategoriesStart());
 
-  try {
-    const categoriesArray = await getCategoriesAndDocuments('categories');
-    dispatch(fetchCategoriesSucceed(categoriesArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFailed(error));
-  }
-}
+//   try {
+//     const categoriesArray = await getCategoriesAndDocuments('categories');
+//     dispatch(fetchCategoriesSucceed(categoriesArray));
+//   } catch (error) {
+//     dispatch(fetchCategoriesFailed(error));
+//   }
+// }
