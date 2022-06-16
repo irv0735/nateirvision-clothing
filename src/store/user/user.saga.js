96b77624bb550a1741be8e7d2365bb4c1c2e7901
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2022-06-16 14:17:29 -0600

    sign-in user converted to saga

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -4,13 +4,33 @@ import { USER_ACTION_TYPES } from "./user.types";
 
 import { signInSuccess, signInFailed } from "./user.action";
 
-import { getCurrentUser, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { getCurrentUser, createUserDocumentFromAuth, signInWithGooglePopup, signInAuthWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
+
+
 
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
   try {
     const userSnapshot = yield call(createUserDocumentFromAuth, userAuth, additionalDetails);
     yield put(signInSuccess({id: userSnapshot.id, ...userSnapshot.data()}))
+  } catch(error) {
+    yield put(signInFailed(error));
+  }
+}
+
+export function* signInWithGoogle() {
+  try {
+    const { user } = yield call(signInWithGooglePopup);
+    yield call(getSnapshotFromUserAuth, user);
+  } catch(error) {
+    yield put(signInFailed(error));
+  }
+}
+
+export function* signInWithEmail({payload: { email, password }}) {
+  try {
+    const { user } = yield call(signInAuthWithEmailAndPassword, email, password);
+    yield call(getSnapshotFromUserAuth, user);
   } catch(error) {
     yield put(signInFailed(error));
   }
@@ -26,11 +46,19 @@ export function* isUserAuthenticated() {
   }
 }
 
+export function* onGoogleSignInStart() {
+  yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle)
+}
+
 export function* onCheckUserSession() {
   yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
+export function* onEmailSignInStart() {
+  yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
 
 export function* userSagas() {
-  yield all([call(onCheckUserSession)]);
+  yield all([call(onCheckUserSession), call(onGoogleSignInStart), call(onEmailSignInStart)]);
 }
